361b250b2aad148293f5ad1174c10b2c8f36966c
Schuyler Seaborn <wsseabor@gmail.com>
2024-02-13 16:03:54 -0500

    parse player's last five games

--- basketballrefapi/last_five.py
+++ basketballrefapi/last_five.py
@@ -1,7 +1,4 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import requests
-import json
+from lxml import html
 
 from data import Location, Outcome, TEAM_ABBREVIATIONS_TO_TEAM
 
@@ -19,7 +16,47 @@ def parse_outcome(symbol):
         return Outcome.LOSS
     raise ValueError(f"Unable to parse {symbol}.")
 
+def parse_games_started(symbol):
+    if symbol == "*":
+        return symbol
+    else:
+        raise ValueError(f"Unable to parse {symbol}.")
+
 def parse_player_last_five(row):
     return {
-        
+        "date": str(row[1].text_content()),
+        "team": TEAM_ABBREVIATIONS_TO_TEAM[row[2].text_content()],
+        "location": parse_location(row[3].text_content()),
+        "opponent": TEAM_ABBREVIATIONS_TO_TEAM[row[4].text_content()],
+        "outcome": parse_outcome(row[5].text_content()),
+        "games_started": parse_games_started(row[6].text_content()),
+        "minutes_played": int(row[7].text_content()),
+        "field_goals": int(row[8].text_content()),
+        "field_goal_attempts": int(row[9].text_content()),
+        "field_goal_percentage": float(row[10].text_content()),
+        "three_points": int(row[11]).text_content(),
+        "three_point_percentage": float(row[12].text_content()),
+        "free_throws": int(row[13].text_content()),
+        "free_throw_attempts": int(row(14).text_content()),
+        "free_throw_percentage": float(row[15].text_content()),
+        "offensive_rebounds": int(row[16].text_content()),
+        "defensive_rebounds": int(row[17].text_content()),
+        "total_rebounds": int(row[18].text_content()),
+        "assists": int(row[19].text_content()),
+        "steals": int(row[20].text_content()),
+        "blocks": int(row[21].text_content()),
+        "turnovers": int(row[22].text_content()),
+        "personal_fouls": int(row[23].text_content()),
+        "points": int(row[24].text_content()),
+        "game_score": float(row[25].text_content()),
+        "plus_minus": float(row[26].text_content()),
     }
+
+def parse_player_last_five(page):
+    tree = html.fromstring(page)
+    rows = tree.xpath('//table[@id="last5"]//tbody/tr[not(contains(@class, "thead"))]')
+    return list(map(lambda row: parse_player_last_five(row), rows))
+
+t = parse_player_last_five("https://www.basketball-reference.com/players/j/jokicni01.html")
+print(t)
+
