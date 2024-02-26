d01d1b2856370a368dc74b0d7448906f50a4321b
Schuyler Seaborn <wsseabor@gmail.com>
2024-02-26 16:21:24 -0500

    Changed to BS4 for scraping

--- basketballrefapi/sandbox.py
+++ basketballrefapi/sandbox.py
@@ -5,9 +5,16 @@ Please don't look at this
 """
 
 import requests
+from bs4 import BeautifulSoup
 from lxml import html
 from lxml import etree
 import csv
+import pandas as pd
+
+
+"""
+
+-- LXML example that is tough to operate -- 
 
 url = "https://www.basketball-reference.com/players/a/antetgi01.html"
 
@@ -18,13 +25,14 @@ rows = []
 tree = html.fromstring(re.content)
 
 headers = tree.xpath('//div[@class="table_container"]//thead')
-#ex = tree.xpath('//div[@class="table_container"]//tbody/tr[not(contains(@class, "thead"))]')
+ex = tree.xpath('//div[@class="table_container"]//tbody/tr[not(contains(@class, "thead"))]')
 
-#ex = tree.xpath('//div[@class="table_container"]//tbody/tr/td[@data-stat="mp"]')
+ex = tree.xpath('//div[@class="table_container"]//tbody/tr/td[@data-stat="mp"]')
+
 
 def split_rows():
 
-    query = '//div[@class="table_container"]//tbody/tr/td[@data-stat="{}"]'
+    query = '//div[@class="table_container"]//tbody/tr/td[@data-stat="{}"]/text()'
 
     paths = []
 
@@ -100,15 +108,59 @@ for i in queries:
     stat = tree.xpath(i)
     
     for row in stat:
-        stats_rows.append(row.text_content())
+        stats_rows.append(row)
 
 with open('test.csv', 'w', newline='') as f:
     writer = csv.writer(f)
 
     writer.writerow(header_rows)
 
-    writer.writerow(stats_rows)
+    for row in range(len(header_rows)):
+        writer.writerow(stats_rows[row])
+"""
 
+"""
 
+-- Beautiful soup example transforming into pandas dataframe
 
+"""
 
+url = "https://www.basketball-reference.com/players/a/antetgi01.html"
+
+re = requests.get(url)
+
+soup = BeautifulSoup(re.text, 'lxml')
+
+table_data = soup.find('table', { 'class' : 'sortable stats_table' })
+
+def table_data_text(table):
+
+    def row_get_data_text(tr, coltag='td'):    
+        return [td.get_text(strip=True) for td in tr.find_all(coltag)]
+
+    rows = []
+    trs = table.find_all('tr')
+    header_row = row_get_data_text(trs[0], 'th')
+
+    if header_row:
+        rows.append(header_row)
+        trs = trs[1:]
+
+    for tr in trs:
+        rows.append(row_get_data_text(tr, 'td'))
+
+    return rows
+
+list_table = table_data_text(table_data)
+print(list_table[:5])
+
+with open('test.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+
+    writer.writerow(list_table[0])
+    writer.writerows(list_table[1:])
+
+"""
+df = pd.DataFrame(list_table[1:], columns = list_table[0])
+df.head(5)
+"""
